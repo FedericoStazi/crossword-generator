a2b953842d4360bcdc23abb2a3c22cfc820eb878
Federico Stazi <34340238+FedericoStazi@users.noreply.github.com>
2020-04-05 18:22:29 +0100

    I/O using temporary files

--- sat_solver.py
+++ sat_solver.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import time
+import volatile
 
 #run sat solver
 def solve(dimacs_cnf):
@@ -20,25 +21,28 @@ def solve(dimacs_cnf):
 #run sat solver given time limit
 def timed_solve(dimacs_cnf, timeout):
 
-    input_file = open("input.txt", "w")
-    input_file.write(dimacs_cnf)
-    input_file.close()
+    with volatile.file(mode = 'w') as input_file, volatile.file(mode = 'r') as output_file:
 
-    #adding exec makes kill work (https://stackoverflow.com/questions/4789837/how-to-terminate-a-python-subprocess-launched-with-shell-true)
-    proc = subprocess.Popen("exec " + "minisat -verb=0 input.txt output.txt > /dev/null", shell=True)
+        input_file.write(dimacs_cnf)
+        input_file.close()
 
-    try:
+        #adding exec makes kill work (https://stackoverflow.com/questions/4789837/how-to-terminate-a-python-subprocess-launched-with-shell-true)
+        proc = subprocess.Popen("exec " + "minisat -verb=0 " + input_file.name + " " + output_file.name + " > /dev/null", shell=True)
 
-        proc.communicate(timeout=timeout)
+        try:
 
-        output_file = open("output.txt", "r")
-        result = output_file.read()
+            proc.communicate(timeout=timeout)
 
-        output_file.close()
-        return result
+            result = output_file.read()
+            output_file.close()
 
-    except subprocess.TimeoutExpired:
+            return result
 
-        proc.kill()
-        proc.communicate()
-        return None
+        except subprocess.TimeoutExpired:
+
+            output_file.close()
+
+            proc.kill()
+            proc.communicate()
+
+            return None
